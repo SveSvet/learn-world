bf5e3d03c1e97a1f1593fd17f45aabad1ce96aab
Sveta <s.tu.tu.908@gmail.com>
2020-05-18 16:42:37 +0300

    Updates

---
+++ precache-manifest.367021c47e803d6e46a4c3a0c949239f.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098ebd789750de80e749ef4ccb6da117",
+    "url": "/learn-world/index.html"
+  },
+  {
+    "revision": "9ad585f36e8fd07511af",
+    "url": "/learn-world/static/css/main.8f475ca0.chunk.css"
+  },
+  {
+    "revision": "a660fd84ddda4d7939eb",
+    "url": "/learn-world/static/js/2.27d174f6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/learn-world/static/js/2.27d174f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad585f36e8fd07511af",
+    "url": "/learn-world/static/js/main.ab75b894.chunk.js"
+  },
+  {
+    "revision": "6b9daca68e9adb203069",
+    "url": "/learn-world/static/js/runtime-main.5c224a04.js"
+  },
+  {
+    "revision": "e849b603e4f5ec8875663421886c99d9",
+    "url": "/learn-world/static/media/1.e849b603.jpeg"
+  },
+  {
+    "revision": "205888b59f1dec2e97b8128bf9f0a4f9",
+    "url": "/learn-world/static/media/10.205888b5.jpg"
+  },
+  {
+    "revision": "02c21ed9d4ccb73edfb96bcc580865fa",
+    "url": "/learn-world/static/media/11.02c21ed9.jpg"
+  },
+  {
+    "revision": "8ca5d654b2c94a24d4716e0c4e8e5904",
+    "url": "/learn-world/static/media/2.8ca5d654.jpg"
+  },
+  {
+    "revision": "43144040731a6cd54dcbd082a591c046",
+    "url": "/learn-world/static/media/4.43144040.jpg"
+  },
+  {
+    "revision": "43e75785911841c4fbab51990530258c",
+    "url": "/learn-world/static/media/5.43e75785.jpg"
+  },
+  {
+    "revision": "8e57133fb982cc2b74d0e4dd5b0ae237",
+    "url": "/learn-world/static/media/6.8e57133f.jpg"
+  },
+  {
+    "revision": "14bea63afd1e452986d942b6667d6fbe",
+    "url": "/learn-world/static/media/7.14bea63a.jpg"
+  },
+  {
+    "revision": "b021c91eae929c2110d1dc085588b3db",
+    "url": "/learn-world/static/media/8.b021c91e.jpg"
+  },
+  {
+    "revision": "773187e8a2557f45f3f8315e4f8cfa3c",
+    "url": "/learn-world/static/media/9.773187e8.jpg"
+  },
+  {
+    "revision": "06de6a496db8ec904878115139ddd59c",
+    "url": "/learn-world/static/media/Jost-Black.06de6a49.ttf"
+  },
+  {
+    "revision": "c95be4b0fee1d5cb9654a79c062ac306",
+    "url": "/learn-world/static/media/Jost-Bold.c95be4b0.ttf"
+  },
+  {
+    "revision": "6b4cf060631db66ac5824d2fcf5367da",
+    "url": "/learn-world/static/media/Jost-ExtraBold.6b4cf060.ttf"
+  },
+  {
+    "revision": "2c35a110c2f144ff7c716f4a4d7d60a8",
+    "url": "/learn-world/static/media/Jost-ExtraLight.2c35a110.ttf"
+  },
+  {
+    "revision": "15b1bb7d6f6279967dd5c70c7ae45e52",
+    "url": "/learn-world/static/media/Jost-Light.15b1bb7d.ttf"
+  },
+  {
+    "revision": "9605c4ec7bc54351941ee79756533b87",
+    "url": "/learn-world/static/media/Jost-Medium.9605c4ec.ttf"
+  },
+  {
+    "revision": "435c89e6bd0ccb074eed8bd3e655d6a5",
+    "url": "/learn-world/static/media/Jost-Regular.435c89e6.ttf"
+  },
+  {
+    "revision": "898d65d07b3ff005f2afa1f917b5f6da",
+    "url": "/learn-world/static/media/background.898d65d0.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-world/static/media/logo.5d5d9eef.svg"
+  }
+]);
